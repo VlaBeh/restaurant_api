33d408f4107bbc8804ba15f58d57eaf3c2ce733e
VlaBeh <166541377+VlaBeh@users.noreply.github.com>
2025-03-10 10:44:01 +0200

    Add models + connection

---
+++ db/models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DateTime
+from sqlalchemy.orm import relationship
+from db.session import engine
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class Restaurant(Base):
+    __tablename__ = "restaurants"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, nullable=False)
+    owner_id = Column(Integer, ForeignKey("employees.id"))
+    menus = relationship("Menu", back_populates="restaurant")
+
+
+class Menu(Base):
+    __tablename__ = "menus"
+    id = Column(Integer, primary_key=True, index=True)
+    day = Column(String, nullable=False)
+    restaurant_id = Column(Integer, ForeignKey("restaurants.id"))
+    restaurant = relationship("Restaurant", back_populates="menus")
+
+
+class Employee(Base):
+    __tablename__ = "employees"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False)
+    role = Column(String, nullable=False)
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
